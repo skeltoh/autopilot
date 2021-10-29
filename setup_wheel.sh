ced50f1b903339c59ec1730f2ce9838304b7b803
Henry <hskelto@emory.edu>
2021-10-29 18:43:22 +0100

    added setup script

---
+++ setup_wheel.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+sudo apt update & sudo apt upgrade -y
+sudo apt install -y \
+    python3-dev \
+    python3-pip \
+    git \
+    libatlas-base-dev \
+    libsamplerate0-dev \
+    libsndfile1-dev \
+    libreadline-dev \
+    libasound-dev \
+    i2c-tools \
+    libportmidi-dev \
+    liblo-dev \
+    libhdf5-dev \
+    libzmq-dev \
+    libffi-dev
+
+
+pip3 install virtualenv
+mkdir ~/.venv
+python3 -m virtualenv ~/.venv/autopilot
+source ~/.venv/autopilot/bin/activate
+mkdir git
+cd git
+git clone https://github.com/skeltoh/autopilot.git
+cd autopilot
+pip3 install -e .
